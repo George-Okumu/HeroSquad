3c92977ec007511f75ed3e5e8f85acc73dd99723
George Okumu <gokumu12@gmail.com>
2020-09-13 06:36:47 +0300

    Declared clear method to clear any instances of squads

--- src/main/java/models/Squad.java
+++ src/main/java/models/Squad.java
@@ -7,13 +7,13 @@ public class Squad {
     private String squadName;
     private int maxSize;
     private String squadCause;
-    private static List<Squad> instances = new ArrayList<>();// Will contain all Squad objects
+    private static List<Squad> squads = new ArrayList<>();// Will contain all Squad objects
 
     public Squad(String squadName, int maxSize, String squadCause) {
         this.squadName = squadName;
         this.maxSize = maxSize;
         this.squadCause = squadCause;
-        instances.add(this);
+        squads.add(this);
     }
 
     public String getSquadName() {
@@ -27,7 +27,11 @@ public class Squad {
     public String getSquadCause() {
         return squadCause;
     }
+
     public static List<Squad> all(){
-        return instances;
+        return squads;
+    }
+    public static void clear() {
+        squads.clear();
     }
 }
